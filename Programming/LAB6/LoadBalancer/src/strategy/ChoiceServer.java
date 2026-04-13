8f1ccdf40f29b862b72fe7ae7163cb1addf05a69
Александр Сивков <502535@niuitmo.ru>
2026-04-13 19:31:00 +0300

    обновил способ выбора из активных серверов

--- Programming/LAB6/LoadBalancer/src/strategy/ChoiceServer.java
+++ Programming/LAB6/LoadBalancer/src/strategy/ChoiceServer.java
@@ -3,19 +3,30 @@ package strategy;
 import util.ServerContext;
 
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class ChoiceServer {
-    private HashMap<InetSocketAddress, ServerContext> mapStatesServers;
+    private final List<ServerContext> servers;
+    private int currentIndex;
 
     public ChoiceServer(HashMap<InetSocketAddress, ServerContext> mapStatesServers) {
-        this.mapStatesServers = mapStatesServers;
+        this.servers = new ArrayList<>(mapStatesServers.values());
+        this.currentIndex = 0;
     }
 
     public ServerContext getActiveServer() {
-        if (mapStatesServers.isEmpty()) return null;
-        for (var server: mapStatesServers.keySet()) {
-            if (mapStatesServers.get(server).getIsOnline()) return mapStatesServers.get(server);
+        if (servers.isEmpty()) return null;
+        int size = servers.size();
+
+        for (int i = 0; i < size; i++) {
+            currentIndex = (currentIndex + 1) % size;
+
+            ServerContext server = servers.get(currentIndex);
+            if (server.getIsOnline()) {
+                return server;
+            }
         }
         return null;
     }
